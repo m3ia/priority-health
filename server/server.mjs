103f261b7220ed16135826e13cc6a36770e02937
m3ia <meianatividad@gmail.com>
2022-10-29 09:35:15 -0700

    Add .env

--- server/server.mjs
+++ server/server.mjs
@@ -1,6 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const API_KEY = process.env.API_KEY;
+const APP_ID = process.env.APP_ID;
+console.log('TEST', 'API_KEY: ', API_KEY, 'APP_ID: ', APP_ID)
 
 const app = express();
 const PORT = 8080;
