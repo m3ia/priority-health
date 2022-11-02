ec63c24733240b39d99a0862bfd0d438fbff6ad4
m3ia <meianatividad@gmail.com>
2022-11-01 23:03:28 -0700

    Send user as a prop

--- server/server.mjs
+++ server/server.mjs
@@ -3,8 +3,21 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import pgPromise from 'pg-promise';
+import { auth } from 'express-openid-connect';
 
 dotenv.config();
+
+// auth0 - TODO - check these
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENTID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+console.log('auth0 config', config);
 const NUTRITIONAL_ANALYSIS_APP_ID = process.env.NUTRITIONAL_ANALYSIS_APP_ID;
 const NUTRITIONAL_ANALYSIS_API_KEY = process.env.NUTRITIONAL_ANALYSIS_API_KEY;
 console.log('TEST', 'API_KEY: ', NUTRITIONAL_ANALYSIS_API_KEY, 'APP_ID: ', NUTRITIONAL_ANALYSIS_APP_ID)
@@ -16,12 +29,25 @@ const db = pgp('postgres://localhost:5432/priority_health');
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(auth(config));
+
 app.listen(PORT, () => {
   console.log(`Hola this server is running on port ${PORT}`);
 });
 app.get('/', (req, res) => {
   res.json('Welcome to the API server!')
 })
+
+// First get with user auth:
+app.get('/api/me', (req, res) => {
+  console.log(req.oidc.isAuthenticated());
+  if (req.oidc.isAuthenticated()) {
+    console.log('oidc user from server', req.oidc.user);
+    res.json(req.oidc.user);
+  } else {
+    res.status(401).json({ error: 'Error in the auth0' });
+  }
+});
 
 // Start all routes like so: app.get('/api/')
 // Get for Nutritional Analysis
