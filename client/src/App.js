3ab0652e2b19903f1663567692f0d53125d58b12
m3ia <meianatividad@gmail.com>
2022-11-10 11:16:02 -0800

    Create Recipe Form and link it in navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { NavBar, Footer, Loading } from './components/home-login';
 import { Home, Profile } from './components/home-login/views';
 import Collections from './components/recipes-collections/Collections';
+import NewRecipeForm from './components/recipes-collections/NewRecipeForm';
 
 import AuthNav from './components/home-login/auth-nav';
 import './App.css';
@@ -39,7 +40,8 @@ const App = () => {
               <Route path="/" element={<Home user={user} />} />
               <Route path="/profile" element={<Profile siteUser={siteUser} />} />
               <Route path="/recipes" element={<Collections siteUser={siteUser} />} />
-              <Route path="/foodList" element={<FoodList siteUser={siteUser} foodView={foodView} setFoodView={setFoodView} />} />
+              <Route path="/food-list" element={<FoodList siteUser={siteUser} foodView={foodView} setFoodView={setFoodView} />} />
+              <Route path="/add-new-recipe" element={<NewRecipeForm siteUser={siteUser} />} />
             </Routes>
           </div>
           <div>
---
+++ client/src/components/recipes-collections/NewRecipeForm.jsx
@@ -0,0 +1,10 @@
+const NewRecipeForm = () => {
+  return (
+    <div className="new-recipe-form-container">
+      New Recipe Form Container
+      <div className="new-recipe-form-div">New Recipe Form Div</div>
+    </div>
+  );
+};
+
+export default NewRecipeForm;
