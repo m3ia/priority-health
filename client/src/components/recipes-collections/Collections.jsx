7cb0378de338fae7602c41615a1db678e6969615
m3ia <meianatividad@gmail.com>
2022-11-10 00:20:01 -0800

    Add light styling to skeleton

--- client/src/components/recipes-collections/Collections.jsx
+++ client/src/components/recipes-collections/Collections.jsx
@@ -1,9 +1,12 @@
 import {useState, useEffect} from "react";
 import CollectionsCard from "./CollectionsCard";
 import CollectionsSearchScroll from "./CollectionsSearchScroll";
+import Recipes from "./Recipes";
 
 const Collections = () => {
   const [collectionsData, setCollectionsData] = useState([]);
+  // TODO:
+  const [collectionFilter, setCollectionFilter] = useState("all-recipes");
 
   const getCollections = () => {
     fetch("/api/collections")
@@ -17,16 +20,30 @@ const Collections = () => {
   }, []);
 
   return (
-    <div className="collections-div">
+    <div className="recipes-collections-container">
       <h1>Recipes + Recipe Collections</h1>
-      <div className="collections-cards-div">
-        <div>All Recipes</div>
-        <div>Uncategorized Recipes</div>
-        <CollectionsSearchScroll collectionsData={collectionsData} />
-        {collectionsData.length > 0 &&
-          collectionsData.map((collection, ind) => {
-            return <CollectionsCard key={ind} collection={collection} />;
-          })}
+      <div className="recipes-collections-div">
+        <div className="collections-div">
+          <div
+            className="recipes-btn"
+            onClick={() => setCollectionFilter("all-recipes")}>
+            All Recipes
+          </div>
+          <div
+            className="recipes-btn"
+            onClick={() => setCollectionFilter("uncategorized-recipes")}>
+            Uncategorized Recipes
+          </div>
+          <CollectionsSearchScroll collectionsData={collectionsData} />
+          {collectionsData.length > 0 &&
+            collectionsData.map((collection, ind) => {
+              return <CollectionsCard key={ind} collection={collection} />;
+            })}
+        </div>
+        <div className="recipes-div">
+          <h2>Recipes</h2>
+          <Recipes />
+        </div>
       </div>
     </div>
   );
---
+++ client/src/components/recipes-collections/Recipes.jsx
@@ -0,0 +1,14 @@
+const Recipes = () => {
+  return (
+    <div className="recipes-container">
+      <div className="recipes-searchbar">seachbar</div>
+      <div className="recipes-view-info">
+        {/* Category: {collection.category} Info: {collection.notes} */}
+        recipes-view-info
+      </div>
+      <div className="recipes-list">recipes list</div>
+    </div>
+  );
+};
+
+export default Recipes;
