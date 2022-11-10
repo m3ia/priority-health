36a8c5863ffefc3a3a53df07803573c33c8ebb8c
m3ia <meianatividad@gmail.com>
2022-11-09 21:08:54 -0800

    Create all recipes and uncat recipes collections

--- client/src/components/recipes-collections/Collections.jsx
+++ client/src/components/recipes-collections/Collections.jsx
@@ -2,13 +2,22 @@ import {useState, useEffect} from "react";
 import CollectionsCard from "./CollectionsCard";
 
 const Collections = () => {
-  const [collectionsData, setCollectionsData] = useState([]);
+  const [collectionsData, setCollectionsData] = useState([
+    {
+      name: "All Recipes",
+      notes: "View all your recipes",
+    },
+    {
+      name: "Uncategorized Recipes",
+      notes: "All uncategorized recipes",
+    },
+  ]);
 
   const getCollections = () => {
     fetch("/api/collections")
       .then((res) => res.json())
       .then((res) => {
-        setCollectionsData([...res]);
+        setCollectionsData((prev) => [...prev, ...res]);
       });
   };
   useEffect(() => {
