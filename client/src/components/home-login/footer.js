95e7e389b4ee04e14ed30941022cef5a5f98f44f
m3ia <meianatividad@gmail.com>
2022-11-09 19:53:15 -0800

    Create Collections page, card, and add vector images

--- client/src/components/home-login/footer.js
+++ client/src/components/home-login/footer.js
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer className="bg-light p-3 text-center">
     <div className="logo" />
     <p>
-      Footer here
+      <a href="https://www.flaticon.com/free-icons/folder" title="folder icons">Folder icons created by Smashicons - Flaticon</a>
     </p>
   </footer>
 );
--- client/src/components/recipes-collections/CollectionsCard.jsx
+++ client/src/components/recipes-collections/CollectionsCard.jsx
@@ -1,11 +1,18 @@
-import recipeIcon from "./recipe-book.png";
-
+import recipeIcon from "./menu.png";
+import collectionsIcon from "./cookbook.png";
 const CollectionsCard = ({collection}) => {
   return (
     <div className="collections-card-div">
       <h2>{collection.name}</h2>
       <div className="collections-card-body">
-        <img src={recipeIcon} alt="recipe-icon" height="75"></img>
+        <img
+          className="recipe-card-icon vector-icons"
+          src={recipeIcon}
+          alt="recipe-icon"></img>
+        <img
+          className="collections-card-icon vector-icons"
+          src={collectionsIcon}
+          alt="collections-icon"></img>
         <p>{collection.notes}</p>
       </div>
     </div>
