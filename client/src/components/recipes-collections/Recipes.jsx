9f6a98df4ddb31c25a59bc7c1602ecfd346b4c00
m3ia <meianatividad@gmail.com>
2022-11-11 21:49:24 -0800

    Style Single Recipe Page

--- client/src/components/recipes-collections/Recipes.jsx
+++ client/src/components/recipes-collections/Recipes.jsx
@@ -4,7 +4,6 @@ import {useNavigate} from "react-router-dom";
 
 const Recipes = ({siteUser}) => {
   const [recipes, setRecipes] = useState([]);
-  const [singleRecipeId, setSingleRecipeId] = useState(null);
 
   const navigate = useNavigate();
 
@@ -18,19 +17,6 @@ const Recipes = ({siteUser}) => {
       });
   };
 
-  // Fetch data for a single recipe
-  // const viewRecipe = async (recipeId) => {
-  //   console.log("im in there");
-  //   await fetch(`/api/recipe/${recipeId}`)
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       console.log("recipeId: ", recipeId);
-  //       console.log("ressssy: ", res);
-  //       setSingleRecipeId(recipeId);
-  //       navigate(`/recipe/${recipeId}`);
-  //     });
-  // };recipeId
-
   const navToSingleRecipeView = (recipeId) => navigate(`/recipe/${recipeId}`);
 
   useEffect(() => {
@@ -43,10 +29,7 @@ const Recipes = ({siteUser}) => {
         <div className="recipes-searchbar">seachbar</div>
         <div className="recipes-form-btn">+</div>
       </div>
-      <div className="recipes-view-info">
-        {/* Category: {collection.category} Info: {collection.notes} */}
-        recipes-view-info
-      </div>
+      <div className="recipes-view-info">recipes-view-info</div>
       <div className="recipe-list-cards-div">
         {recipes.map((recipe, ind) => {
           return (
