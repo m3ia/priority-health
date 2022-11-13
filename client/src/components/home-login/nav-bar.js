7fdc9eaf2eea92b1035ff995ae5ef336d50aae83
m3ia <meianatividad@gmail.com>
2022-11-13 13:26:48 -0800

    Center Single Recipe View page

--- client/src/components/home-login/nav-bar.js
+++ client/src/components/home-login/nav-bar.js
@@ -7,7 +7,7 @@ const NavBar = ({setFoodView}) => {
   return (
     <div className="nav-container mb-3">
       <nav className="navbar navbar-expand-md navbar-light bg-light">
-        <div className="container">
+        <div className="nav-inner-container">
           <div className="navbar-brand logo" />
           <MainNav setFoodView={setFoodView} />
           <AuthNav />
