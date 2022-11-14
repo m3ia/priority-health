0af9659dd5c75b65ce479c8c21914e68b74674bb
m3ia <meianatividad@gmail.com>
2022-11-13 22:03:08 -0800

    Add filter for recipe cards

--- client/src/components/recipes-collections/Recipes.jsx
+++ client/src/components/recipes-collections/Recipes.jsx
@@ -4,6 +4,7 @@ import {useNavigate} from "react-router-dom";
 
 const Recipes = ({siteUser}) => {
   const [recipes, setRecipes] = useState([]);
+  const [searchString, setSearchString] = useState("");
 
   const navigate = useNavigate();
 
@@ -25,20 +26,44 @@ const Recipes = ({siteUser}) => {
   return (
     <div className="recipes-container">
       <div className="recipes-search-form-div">
-        <div className="recipes-searchbar">seachbar</div>
+        <div className="recipes-searchbar">
+          <input
+            type="text"
+            placeholder="Search for a recipe"
+            value={searchString}
+            onChange={(e) => {
+              setSearchString(e.target.value);
+            }}></input>
+        </div>
         <div className="recipes-form-btn">+</div>
       </div>
       <div className="recipes-view-info">recipes-view-info</div>
       <div className="recipe-list-cards-div">
-        {recipes.map((recipe, ind) => {
-          return (
-            <RecipeListCard
-              key={ind}
-              recipe={recipe}
-              navToSingleRecipeView={navToSingleRecipeView}
-            />
-          );
-        })}
+        {searchString.length > 0
+          ? recipes
+              .filter(
+                (recipe) =>
+                  recipe.name.includes(searchString) ||
+                  recipe.url.includes(searchString)
+              )
+              .map((recipe, ind) => {
+                return (
+                  <RecipeListCard
+                    key={ind}
+                    recipe={recipe}
+                    navToSingleRecipeView={navToSingleRecipeView}
+                  />
+                );
+              })
+          : recipes.map((recipe, ind) => {
+              return (
+                <RecipeListCard
+                  key={ind}
+                  recipe={recipe}
+                  navToSingleRecipeView={navToSingleRecipeView}
+                />
+              );
+            })}
       </div>
     </div>
   );
