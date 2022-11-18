2bebb481d2ad75d3d59eb87ca09b9cc5e0d08dff
m3ia <meianatividad@gmail.com>
2022-11-18 13:48:06 -0800

    Remove menu btns from home

--- client/src/components/home/home.jsx
+++ client/src/components/home/home.jsx
@@ -11,7 +11,7 @@ const Home = ({user, siteUser}) => {
       <div className="text-center hero home-container">
         <h1>Welcome, {user.given_name}!</h1>
         <div className="home-section-1">
-          <div className="home-menu-btns">
+          {/* <div className="home-menu-btns">
             <div className="menu-btns" onClick={() => navigate("/food-list")}>
               Food Tolerance List
             </div>
@@ -28,32 +28,47 @@ const Home = ({user, siteUser}) => {
               onClick={() => navigate("/add-new-food")}>
               Add New Food
             </div>
+          </div> */}
+          <div className="home-section-left">
+            <div className="user-diet-info">
+              <div>
+                Current Allergies: <input></input>
+              </div>
+              <div>
+                Current Dietary Preferences: <input></input>
+              </div>
+              <div>
+                Current Dietary Restrictions: <input></input>
+              </div>
+            </div>
+            <div className="meal-check-in-div">
+              <div>
+                <p className="check-in-text">How I felt since my last meal:</p>
+                <p className="check-in-btns-p">
+                  <span className="check-in-btn">😋</span>
+                  <span className="check-in-btn">😐</span>
+                  <span className="check-in-btn">😔</span>
+                </p>
+              </div>
+              <div>
+                Meal: <input></input>
+              </div>
+              <div>
+                Notes: <textarea></textarea>
+              </div>
+              <button>submit</button>
+            </div>
           </div>
+
           <div>
+            <h2>Most Recent Recipes</h2>
             <FiveRecCards siteUser={siteUser} />
           </div>
         </div>
-        <div className="home-section-2">
-          <div>
-            Current Allergies: <input></input>
-          </div>
-          <div>
-            Current Dietary Preferences: <input></input>
-          </div>
-          <div>
-            Current Dietary Restrictions: <input></input>
-          </div>
-        </div>
+
+        <div className="home-section-2"></div>
         <div className="home-section-3">
           <h3>Food Log</h3>
-          <div className="meal-check-in-div">
-            <span className="check-in-text">
-              How I felt since my last meal:
-            </span>
-            <span className="check-in-btn">😋</span>
-            <span className="check-in-btn">😐</span>
-            <span className="check-in-btn">😔</span>
-          </div>
         </div>
       </div>
     </Fragment>
