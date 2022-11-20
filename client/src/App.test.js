96f5b861883f99bd3aa5fe7710ad51a1c027b4fa
m3ia <meianatividad@gmail.com>
2022-11-19 18:04:54 -0800

    Add tests with mock for auth0

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("@auth0/auth0-react", () => ({
+  useAuth0: () => ({
+    isAuthenticated: false,
+    user: null,
+    logout: jest.fn(),
+    loginWithRedirect: jest.fn(),
+    getAccessTokenWithPopup: jest.fn(),
+    getAccessTokenSilently: jest.fn(),
+    getIdTokenClaims: jest.fn(),
+    loginWithPopup: jest.fn(),
+    isLoading: false,
+  }),
+  withAuthenticationRequired: (component) => component
+}));
+
+describe("App", () => {
+  test("renders App component", () => {
+    render(<App />);
+  });
 });
+
+test("renders logo", async () => {
+  
+  render(<App />);
+  const logo = screen.getByText(/priorityHealth/i);
+  
+  // eslint-disable-next-line jest/valid-expect
+  expect(logo);
+})
