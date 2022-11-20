c255d37ba334654ba0a4e8d6b5c8c535ed7d5d17
m3ia <meianatividad@gmail.com>
2022-11-19 21:35:07 -0800

    Resolve warnings

--- client/src/components/home-login/main-nav.js
+++ client/src/components/home-login/main-nav.js
@@ -1,67 +1,67 @@
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import React from "react";
-import { useAuth0 } from '@auth0/auth0-react';
-
+import {useAuth0} from "@auth0/auth0-react";
 
 const MainNav = ({setFoodView}) => {
-  const { logout } = useAuth0();
+  const {logout} = useAuth0();
 
   return (
     <div className="navbar-nav mr-auto">
       <NavLink
         to="/"
-        exact
-        className="nav-link"
-        activeClassName="router-link-exact-active"
-      >
+        exact='true'
+        className={(navData) =>
+          navData.isActive ? "router-link-exact-active" : "nav-link"
+        }>
         Home
       </NavLink>
       <NavLink
         to="/food-list"
         onClick={() => setFoodView("")}
-        exact
-        className="nav-link"
-        activeClassName="router-link-exact-active"
-      >
+        exact='true'
+        className={(navData) =>
+          navData.isActive ? "router-link-exact-active" : "nav-link"
+        }>
         Food List
       </NavLink>
       <NavLink
         to="/add-new-recipe"
-        exact
-        className="nav-link"
-        activeClassName="router-link-exact-active"
-      >
+        exact='true'
+        className={(navData) =>
+          navData.isActive ? "router-link-exact-active" : "nav-link"
+        }>
         Add A Recipe
       </NavLink>
       <NavLink
         to="/recipes"
-        exact
-        className="nav-link"
-        activeClassName="router-link-exact-active"
-      >
+        exact='true'
+        className={(navData) =>
+          navData.isActive ? "router-link-exact-active" : "nav-link"
+        }>
         Recipes
       </NavLink>
       <NavLink
         to="/profile"
-        exact
-        className="nav-link"
-        activeClassName="router-link-exact-active"
-      >
+        exact='true'
+        className={(navData) =>
+          navData.isActive ? "router-link-exact-active" : "nav-link"
+        }>
         Profile
       </NavLink>
       <NavLink
-        exact
-        className="nav-link"
-        activeClassName="router-link-exact-active"
-        onClick={() => 
+        exact='true'
+        className={(navData) =>
+          navData.isActive ? "router-link-exact-active" : "nav-link"
+        }
+        onClick={() =>
           logout({
             returnTo: window.location.origin,
           })
-        }
-      >
+        }>
         Log Out
       </NavLink>
-    </div>);
+    </div>
+  );
 };
 
 export default MainNav;
